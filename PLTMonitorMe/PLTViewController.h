e78c13ec2171df2475c4add59cd8ff69129370ba
Sairam Sankaran <sairam.sankaran@gmail.com>
2014-02-08 10:06:03 -0800

    First commit - Concept 1 setup

---
+++ PLTMonitorMe/PLTViewController.h
@@ -0,0 +1,13 @@
+//
+//  PLTViewController.h
+//  PLTMonitorMe
+//
+//  Created by Sairam Sankaran on 2/7/14.
+//  Copyright (c) 2014 Sairam Sankaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLTViewController : UIViewController
+
+@end
---
+++ PLTMonitorMe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PLTMonitorMe
+//
+//  Created by Sairam Sankaran on 2/7/14.
+//  Copyright (c) 2014 Sairam Sankaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
